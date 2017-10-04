43e366aaab5645f266d080ca89987903ae5424c7
la1perry <lperry1884@gmail.com>
2017-10-04 22:23:45 +0000

    commit message

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const fs=require('fs');
 let dir='./posts';
 let build='./build';
+let md=require('node-markdown').Markdown;
 const path=require('path');
 var cache=[];
 let counter=1;
@@ -12,14 +13,17 @@ fs.readdir(dir,(err,files)=>{
     fs.readFile(`${dir}/${file}`,'utf8',(err, data)=>{
       if(err)console.log(err);
 
-      let template=`
+      let input=md(data.toString());
+      let template= `
       <html>
       <body>
-      <div>${data}</div>
-      </body></html>
-      `;
-      
-      fs.writeFile('build/page'+`${counter}`+'.html',template.trim(), 'utf8', err=>{
+      <div>
+      ${input}
+      </div>
+      </body>
+      </html>
+      `
+      fs.writeFile('build/post'+`${counter}`+'.html',template.trim(), 'utf8', err=>{
         if(err) console.log(err);
       });
       // endwrite
@@ -37,27 +41,17 @@ fs.readdir(build,(err,files)=>{
   files.forEach((file)=>{
     let loc=(path.basename(file));
     cache.push(loc);
+    // console.log(cache);
   });
   // endforeach
-
-// var input=cache.toString();
-// var newchar ="";
-// input=input.split(',').join(newchar);
-// console.log(input);
-
-// var list=[];
-// for(let i=0;i<input.length;i++){
-// list.push ('<li>'+JSON.stringify(input[i])+'</li>')
-// }
+  cache.splice(0,1);
 
 var links=[];
 for(let i=0;i<cache.length;i++){
   links.push('<li><a href="'+cache[i]+'">'+cache[i]+'</a></li>');
 }
-  console.log(links);
 
- var list=links.join(" ");
-
+var list=links.join(" ");
 
 var index=`
       <html>
